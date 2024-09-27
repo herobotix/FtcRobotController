569b8dc28129a8386199f68eb0a6966d7294cdd6
justinjschaub <justinjschaub@gmail.com>
2024-09-27 09:14:18 -0700

    Moved Beanie Bots 2023 Code to an Exclude Folder

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/2023/auto/autoRedMain.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/2023/auto/autoRedMain.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.team22258;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
