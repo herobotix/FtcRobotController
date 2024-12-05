211f022ec22ba5b46f20035a3f3158830fd815a1
Adam Thompson <thompson.adams@gmail.com>
2024-12-04 21:16:44 -0700

    Update NEW to include Arm/Intake Control

--- Team00000/src/main/java/org/firstinspires/ftc/team00000/teleop/NEW.java
+++ Team00000/src/main/java/org/firstinspires/ftc/team00000/teleop/NEW.java
@@ -16,54 +16,65 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.team00000.ChassisMecanum;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+import java.util.Arrays;
+import java.util.List;
+
 @TeleOp(name="NEW", group="teleop")
 //@Disabled
 public class NEW extends LinearOpMode {
+    // Declare the hardware variables.
     private DcMotorEx BLMotor;
     private DcMotorEx BRMotor;
     private DcMotorEx FLMotor;
     private DcMotorEx FRMotor;
+    private DcMotorSimple ArmMotor;
+    private DcMotorSimple IntakeArmMotor;
+    private Servo ClawServo;
+    private CRServo IntakeServo;
     private IMU imu;
 
-    private double integralSum = 0;
-    private double Kp = 0;
-    private double Ki = 0;
-    private double Kd = 0;
-
-    ElapsedTime timer = new ElapsedTime();
-    private double lastError = 0;
-
-    //initializing stick axis and power level variables
+    // Declare OpMode Variables
     private double LSx, rLSx, LSy, rLSy, RSx;
     private double FRPwr, BRPwr, FLPwr, BLPwr, MaxPwr;
+    private double ArmKf;
+    private boolean ClawOpen, ClawChange;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // Initialize the hardware variables.
+        // Map the hardware variables.
         FLMotor  = hardwareMap.get(DcMotorEx.class, "FLMotor");
         BLMotor = hardwareMap.get(DcMotorEx.class, "BLMotor");
         FRMotor  = hardwareMap.get(DcMotorEx.class, "FRMotor");
         BRMotor = hardwareMap.get(DcMotorEx.class, "BRMotor");
+        ArmMotor= hardwareMap.get(DcMotorSimple.class, "ArmMotor");
+        IntakeArmMotor= hardwareMap.get(DcMotorSimple.class, "IntakeArmMotor");
+        ClawServo= hardwareMap.get(Servo.class, "ClawServo");
+        IntakeServo= hardwareMap.get(CRServo.class, "IntakeServo");
         imu = hardwareMap.get(IMU.class,"imu");
 
-        // Stop and reset encoders
+        // Initialize the hardware variables.
+        //     Stop, reset, and enable wheel motor encoders.
         FLMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BLMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FRMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BRMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        
-        // Set motors to use encoders
         FLMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BLMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BLMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FRMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         FRMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BRMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); 
+        BRMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BRMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        imu.initialize(new IMU.Parameters(
-                new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
-                        RevHubOrientationOnRobot.UsbFacingDirection.LEFT
-                )
-        ));
+        //     Set Claw State
+        ClawOpen=false;
+        ClawChange=true;
+
+        //     Set Intake State
+        IntakeServo.setDirection(CRServo.Direction.REVERSE);
+
+        //     Set IMU directions of Logo and USB port
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(
+            RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+            RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+        )));
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -80,28 +91,43 @@ public class NEW extends LinearOpMode {
             rLSy=LSx*Math.sin(-botHeading)+LSy*Math.cos(-botHeading);
             RSx=-gamepad1.right_stick_x;
 
-            telemetry.addData("LSy", LSy);
-            telemetry.addData("LSx", LSx);
-            telemetry.addData("RSx", RSx);
-            
             MaxPwr=Math.max(Math.abs(rLSx)+Math.abs(rLSy)+Math.abs(RSx), 1);
-            // Change MaxPwr to allow higher average power ... find max of FLPwr, BLPwr, FRPwr, BRPwr, 1
             FLPwr=(rLSy+rLSx+RSx)/MaxPwr;
             BLPwr=(rLSy-rLSx+RSx)/MaxPwr;
             FRPwr=(rLSy-rLSx-RSx)/MaxPwr;
             BRPwr=(rLSy+rLSx-RSx)/MaxPwr;
-            
+
              // Handle state transitions based on gamepad input
             FLMotor.setPower(FLPwr); 
             BLMotor.setPower(BLPwr);
             FRMotor.setPower(FRPwr); 
-            BRMotor.setPower(BRPwr); 
-            
-            telemetry.update();
-        }
-       
-       
-  
-    }
+            BRMotor.setPower(BRPwr);
 
+            //Raise and lower the Arm
+            ArmMotor.setPower(-gamepad1.right_stick_y);
+
+            //Raise and lower the Intake Arm
+            if(gamepad1.left_trigger>0) IntakeArmMotor.setPower(gamepad1.left_trigger);
+            else if(gamepad1.right_trigger>0) IntakeArmMotor.setPower(-gamepad1.right_trigger);
+            else IntakeArmMotor.setPower(0.0);
+
+            //Open and Close Claw
+            if(gamepad1.a && ClawChange) {
+                if(ClawOpen) {
+                    ClawServo.setPosition(0.7);
+                    ClawOpen=false;
+                } else{
+                    ClawServo.setPosition(0.4);
+                    ClawOpen=true;
+                }
+                ClawChange=false;
+            } else if (!gamepad1.a && !ClawChange) ClawChange=true;
+
+            if(gamepad1.left_bumper) IntakeServo.setPower(-1);
+            else if(gamepad1.right_bumper) IntakeServo.setPower(1);
+            else IntakeServo.setPower(0);
+
+            //telemetry.update();
+        }
+    }
 }
