34a7e6480473f18f76119144a9896d55bd026d13
Lucas-183119 <lucas.183119@hastudents.us>
2024-12-10 17:30:32 -0700

    Updated Code to latest Ver.

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/teleop/opmode (TeleOp) [1.0.12].java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/teleop/opmode (TeleOp) [1.0.12].java
@@ -8,23 +8,19 @@ import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
 
 /*
-Reorgnized code and renamed functions.
+Removed Lift as it is no longer part of the robot.
 */
 
-
-
-@TeleOp(name = "Opmode (TeleOp) [1.0.9]")
-public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
+@TeleOp(name = "Opmode (TeleOp) [1.0.12]")
+public class opmode__TeleOp___1_0_12_ extends LinearOpMode {
 
   private DcMotor Arm;
   private DcMotor FLMotor;
   private DcMotor FRMotor;
   private DcMotor BLMotor;
   private DcMotor BRMotor;
-  private DcMotor Lift;
   private Servo LClaw;
   private Servo RClaw;
-  private Servo Wrist;
 
   double ArmInput;
   int ArmState;
@@ -33,10 +29,6 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
   int ClawState;
   int Clawn;
   
-  int WristState;
-  int Wriston;
-  
-  int ArmLiftPower;
   int ArmTiltPower;
   
   int FLMotorPower;
@@ -44,12 +36,7 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
   int BLMotorPower;
   int BRMotorPower;
 
-  /**
-   * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue Comment
-   * Blocks show where to place Initialization code (runs once, after touching the DS INIT
-   * button, and before touching the DS Start arrow), Run code (runs once, after touching
-   * Start), and Loop code (runs repeatedly while the OpMode is active, namely not Stopped).
-   */
+  
   @Override
   public void runOpMode() {
     Arm = hardwareMap.get(DcMotor.class, "Arm");
@@ -57,15 +44,15 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
     FRMotor = hardwareMap.get(DcMotor.class, "FRMotor");
     BLMotor = hardwareMap.get(DcMotor.class, "BLMotor");
     BRMotor = hardwareMap.get(DcMotor.class, "BRMotor");
-    Lift = hardwareMap.get(DcMotor.class, "Lift");
     LClaw = hardwareMap.get(Servo.class, "LClaw");
     RClaw = hardwareMap.get(Servo.class, "RClaw");
-    Wrist = hardwareMap.get(Servo.class, "Wrist");
 
-      Startup();
+      F_IMU();
+	  F_Startup();
       waitForStart();
     if (opModeIsActive()) {
       while (opModeIsActive()) {
+		F_Misc();
         F_Move();
         F_Clawrm();
         F_Telemetry();
@@ -74,16 +61,35 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
     }
   }
 
+/**
+   * Describe this function...
+   */
+  private void F_IMU() {
+	// Retrieve the IMU from the hardware map
+	IMU imu = hardwareMap.get(IMU.class, "imu");
+	// Adjust the orientation parameters to match your robot
+	IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+			RevHubOrientationOnRobot.LogoFacingDirection.UP,
+			RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+	// Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+	imu.initialize(parameters);
+  }
+
   /**
    * Describe this function...
    */
-  private void Startup() {
+  private void F_Startup() {
     Arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     ClawState = 0;
-    WristState = 0;
     ArmState = 0;
     Clawn = 0;
-    Wriston = 0;
+	Armon = 0;
+  }
+
+  private void F_Misc() {
+    if (gamepad1.options) { //Reset facing direction to robot's orientation.
+		imu.resetYaw();
+	}
   }
 
   /**
@@ -115,7 +121,7 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
     }
     if (true) {
       // Power Control
-      MotorPowerNormalizer = ((Double) JavaUtil.inListGet(JavaUtil.sort(JavaUtil.createListWith(Math.abs(gamepad1.left_stick_x), Math.abs(gamepad1.left_stick_y), Math.abs(gamepad1.right_stick_x)), JavaUtil.SortType.NUMERIC, JavaUtil.SortDirection.DESCENDING), JavaUtil.AtMode.FIRST, (int) 0, false)).doubleValue() / ((Double) JavaUtil.inListGet(JavaUtil.sort(JavaUtil.createListWith(Math.abs(FLMotorPower), Math.abs(FRMotorPower), Math.abs(BLMotorPower), Math.abs(BRMotorPower)), JavaUtil.SortType.NUMERIC, JavaUtil.SortDirection.DESCENDING), JavaUtil.AtMode.FIRST, (int) 0, false)).doubleValue();
+      MotorPowerNormalizer = ( Math.max( Math.abs(gamepad1.left_stick_x), Math.abs(gamepad1.left_stick_y), Math.abs(gamepad1.right_stick_x) ) / ( (double) Math.max( Math.abs(FLMotorPower), Math.abs(FRMotorPower), Math.abs(BLMotorPower), Math.abs(BRMotorPower) ) ) );
       FLMotorPower = (int) (FLMotorPower * MotorPowerNormalizer);
       FRMotorPower = (int) (FRMotorPower * MotorPowerNormalizer);
       BLMotorPower = (int) (BLMotorPower * MotorPowerNormalizer);
@@ -128,7 +134,6 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
    */
   private void F_Clawrm() {
     F_Clawggle();
-    F_Wristoggle();
     F_Arm();
   }
 
@@ -159,49 +164,21 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
   }
 
   /**
-   * Describe this function...
-   */
-  private void F_Wristoggle() {
-    if (WristState == 0 && gamepad2.right_bumper) {
-      // claw closed
-      WristState = 1;
-      // claw open
-    } else if (WristState == 1 && !gamepad2.right_bumper) {
-      WristState = 2;
-      // claw open
-    } else if (WristState == 2 && gamepad2.right_bumper) {
-      // claw open
-      WristState = 3;
-      // claw closed
-    } else if (WristState == 3 && !gamepad2.right_bumper) {
-      WristState = 0;
-      // claw closed
-    }
-    if (WristState == 0 || WristState == 3) {
-      Wriston = 0;
-    } else {
-      Wriston = 1;
-    }
-  }
-
-  /**
-   * Describe this function...
+   * Moves Arm
    */
   private void F_Arm() {
     ArmInput = gamepad2.left_trigger - gamepad2.right_trigger;
     if (ArmInput != 0) {
-      F_ToggleArm();
+      F_NewWriston();
       ArmTiltPower += ArmInput * (Armon == 1 ? 1 : 0.8);
       // Turn Arm
     }
-    ArmLiftPower += gamepad2.dpad_up ? 1 : (gamepad2.dpad_down ? -1 : 0);
-    // Lift Arm
   }
 
   /**
-   * Describe this function...
+   * Toggles Arm
    */
-  private void F_ToggleArm() {
+  private void F_NewWriston() {
     if (ArmState == 0 && gamepad2.dpad_left) {
       // claw closed
       ArmState = 1;
@@ -232,15 +209,11 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
     telemetry.addData("▼", gamepad1.dpad_down ? 1 : 0);
     telemetry.addData("◄", gamepad1.dpad_left ? 1 : 0);
     telemetry.addData("ClawState", ClawState);
-    telemetry.addData("WristState", WristState);
     telemetry.addData("ArmState", ArmState);
     telemetry.addData("Clawn", Clawn);
-    telemetry.addData("Wriston", Wriston);
     telemetry.addData("ArmInput", ArmInput);
     telemetry.addData("Armon", Armon);
     telemetry.addData("ArmTiltPower", ArmTiltPower);
-    telemetry.addData("ArmLiftPower", ArmLiftPower);
-    telemetry.addData("WristPos", Wrist.getPosition());
     telemetry.addData("LClawPos", LClaw.getPosition());
     telemetry.addData("RClawPos", RClaw.getPosition());
     telemetry.F_Update();
@@ -256,14 +229,11 @@ public class opmode__TeleOp___1_0_9_ extends LinearOpMode {
     BLMotor.setPower(BLMotorPower * -1);
     BRMotor.setPower(BRMotorPower * -1);
     Arm.setPower(ArmTiltPower);
-    Lift.setPower(ArmLiftPower * -1);
     LClaw.setPosition(0 == Clawn ? 0.75 : 0.25);
     RClaw.setPosition(1 == Clawn ? 0.75 : 0.25);
-    Wrist.setPosition(1 == Wriston ? 1 : 0.7);
     FLMotorPower = 0;
     FRMotorPower = 0;
     BLMotorPower = 0;
     BRMotorPower = 0;
     ArmTiltPower = 0;
-    ArmLiftPower = 0;
   }
