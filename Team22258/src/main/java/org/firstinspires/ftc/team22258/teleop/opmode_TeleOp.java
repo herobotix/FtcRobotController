1d595cdaa27f51702b6a4e4aaa5e53cdff33914c
Lucas-183119 <lucas.183119@hastudents.us>
2025-01-07 17:27:07 -0700

    Added EVEN MORE Telemetry

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/teleop/opmode_TeleOp.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/teleop/opmode_TeleOp.java
@@ -212,6 +212,9 @@ public class opmode_TeleOp extends LinearOpMode {
    * Describe this function...
    */
   private void F_Telemetry() {
+	telemetry.addData("LStickX", left_stick_x);
+	telemetry.addData("LStickY", left_stick_y);
+	telemetry.addData("RStickX", right_stick_x);
 	telemetry.addData("MPN", MotorPowerNormalizer);
 	telemetry.addData("FLMotorPower", FLMotorPower);
 	telemetry.addData("FLMotorPower", FRMotorPower);
