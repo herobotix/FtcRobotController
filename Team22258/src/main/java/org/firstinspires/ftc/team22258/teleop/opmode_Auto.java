2e263a560f065f3449e48debf024c75e888fba6e
Lucas-183119 <lucas.183119@hastudents.us>
2025-01-17 13:47:34 -0700

    Made Autonomous ... Autonomous. | Auto

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/teleop/opmode_Auto.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/teleop/opmode_Auto.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
