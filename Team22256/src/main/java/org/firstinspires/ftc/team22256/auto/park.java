321cc41e8d8d580ee1262388d95ff6bf7dd857cb
Armani Melo <Explodinglego3@gmail.com>
2025-02-05 18:02:13 -0700

    stuff

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/auto/park.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/auto/park.java
@@ -13,130 +13,76 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import org.firstinspires.ftc.team22256.methods.robot;
+
+import org.firstinspires.ftc.team22256.methods.Robot;
+
 @Config
 @Autonomous
 
 
 public class park extends LinearOpMode {
 
-    robot robot = new robot(hardwareMap);
+    Robot robot;
 
     private PIDController controller0;
-    public static double p=0.05,i=0,d=0;
+    public static double p = 0.05, i = 0, d = 0;
     public static double f = -0.05;
     double pid = 0;
     double ff = 0;
     double power = 0;
 
-    public enum lift_state{
-        START,
-        INTAKING,
-        TRANSFERRING,
-        SCORING,
-        RESET
-    }
-    final int INTAKING_POS = 1;//These are both for the motor positions
-    final int TRANSEFFERING_POS = 2;
-
-    final double S1_OUT = 1;
-    final double S2_OUT = 1;
-    final double S1_IN = 2;
-    final double S2_IN = 2;
-
-    final double WRIST_TRANSFER = 1;
-    final double WRIST_SCORING = 2;
-    final double WRIST_IDLE = 3;
-
-    final double CLAW_OPEN = 1;
-    final double CLAW_CLOSE = 2;
-
-    final double FLAPPER_POWER = 0.25;
-
-    final double EXTENDED = -4100;
-    final double RETRACTED = -100;
-    public enum auto{
-        STRAFE,
-        FORWARD,
-        TOUCH
-    }
-    auto Auto   = auto.STRAFE  ;
     public static double ticks_in_degree = 5.9744;
+
     @Override
     public void runOpMode() {
-
+        robot = new Robot(hardwareMap);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        controller0  = new PIDController(p,i,d); // Initialization of controller and coeffients
+        controller0 = new PIDController(p, i, d); // Initialization of controller and coeffients
 
         pid = 0;
         ff = 0;
         power = 0;
 
-
-
-        // Wait for the game to start (driver presses START)
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        waitForStart();
-        robot.slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        robot.rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
 
-        robot.rightBack.setTargetPosition(0);
-        robot.leftBack.setTargetPosition(0);
+
         robot.leftFront.setTargetPosition(0);
         robot.rightFront.setTargetPosition(0);
+        robot.leftBack.setTargetPosition(0);
+        robot.rightBack.setTargetPosition(0);
 
-        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         robot.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        robot.rightBack.setPower(1);
-        robot.leftBack.setPower(1);
-        robot.leftFront.setPower(1);
-        robot.rightFront.setPower(1);
+        robot.leftFront.setVelocity(1500);
+        robot.rightFront.setVelocity(1500);
+        robot.leftBack.setVelocity(1500);
+        robot.rightBack.setVelocity(1500);
 
-        double target = 0;
         while (opModeIsActive()) {
 
-        switch (Auto){
-            case STRAFE:
-                robot.strafeToPosition(0.75,72);
-                Auto = auto.FORWARD;
-                break;
-            case FORWARD:
-                target = -400;
-                Auto = auto.TOUCH;
-                break;
-            case TOUCH:
-                robot.driveToPosition(20,0.25);
-        }
-
-            controller0.setPID(p,i,d);
-            int slidePos = robot.slide.getCurrentPosition();
-            pid = controller0.calculate(slidePos,target);
-            ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
-            power = pid + ff;
-            robot.slide.setPower(power);
+            ;
+            if (opModeIsActive()) {
+                robot.strafeToPosition(1500, 50);
 
 
+                telemetry.addData("power", robot.leftFront.getPower());
+                telemetry.addData("power2", robot.leftBack.getPower());
 
-
-
-
-
-
-            telemetry.update();
+            }
         }
     }
 }
