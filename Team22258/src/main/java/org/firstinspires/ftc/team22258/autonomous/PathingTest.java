298514d3d8a8168235a39534869f7c9aa2ed7068
Lucas-183119 <lucas.183119@hastudents.us>
2025-02-11 18:01:14 -0700

    Added Chamber Position & Altered Comment Syntax

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/autonomous/PathingTest.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/autonomous/PathingTest.java
@@ -11,7 +11,11 @@ import org.firstinspires.ftc.team22258.roadrunner.MecanumDrive;
 @Config
 @Autonomous(name = "PathingTest (Auto) [1.0.1]", group="autonomous")
 public final class PathingTest extends LinearOpMode {
-    public static double startX=16,startY=-66,nearY=-59, farY=-16, mark1X=54, mark2X=65, mark3X=78, upX=38;
+    public static double
+        startX= 16, startY= -66,
+        ChamberX= 0, ChamberY= -45,
+        upX= 38, nearY= -59, farY= -16,
+        mark1X= 54, mark2X= 65, mark3X= 77;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -21,22 +25,33 @@ public final class PathingTest extends LinearOpMode {
 
         waitForStart();
 
-        Actions.runBlocking(
-            drive.actionBuilder(beginPose)
-                .setTangent(0)
-                .splineToConstantHeading(new Vector2d(upX, farY), Math.PI / 2)
-                .strafeTo(new Vector2d(mark1X, farY))
+        // Setup & Run Actions
+            Actions.runBlocking(drive.actionBuilder(beginPose)
+                // Move to Chamber
+                    .strafeTo(new Vector2d(ChamberX, ChamberY))
+
+                // Move to Start Pos.
+                    .strafeTo(new Vector2d(startX, startY))
+
+                // Spline to the (Up, Far) Position
+                    .setTangent(0)
+                    .splineToConstantHeading(new Vector2d(upX, farY), Math.PI / 2)
+
+                // Move To & Push "Sample" 1 into Observation Zone
+                    .strafeTo(new Vector2d(mark1X, farY))
                     .strafeTo(new Vector2d(mark1X, nearY))
                     .strafeTo(new Vector2d(mark1X, farY))
-                .strafeTo(new Vector2d(mark2X, farY))
+
+                // Move To & Push "Sample" 2 into Observation Zone
+                    .strafeTo(new Vector2d(mark2X, farY))
                     .strafeTo(new Vector2d(mark2X, nearY))
                     .strafeTo(new Vector2d(mark2X, farY))
-                .strafeTo(new Vector2d(mark3X, farY))
+
+                // Move To & Push "Sample" 3 into Observation Zone
+                    .strafeTo(new Vector2d(mark3X, farY))
                     .strafeTo(new Vector2d(mark3X, nearY))
                     .strafeTo(new Vector2d(mark3X, farY))
-                .strafeTo(new Vector2d(upX, farY))
-                //.splineTo(new Vector2d(30, 30), Math.PI / 2)
-                //.splineToLinearHeading(new Pose2d(0, 60, 0), Math.PI / 2)
-                .build());
+
+                .build()); // Begin Loop
     }
 }
