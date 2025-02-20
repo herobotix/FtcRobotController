9bd4023b166d03315d359a8fc4a95eb3f79c2896
Lucas-183119 <lucas.183119@hastudents.us>
2025-02-19 18:30:41 -0700

    Redesigned & Updated (ff)'s

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/autonomous/PathTest.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/autonomous/PathTest.java
@@ -13,39 +13,43 @@ import org.firstinspires.ftc.team22258.roadrunner.MecanumDrive;
 @Autonomous(name = "PathTest [1.0.0]", group="autonomous")
 public final class PathTest extends LinearOpMode {
     public static double
-      // Start Position
+        // Start Position
         startX = 0,
         startY = 0,
+        startH = 0,
         
-      // Deltas
+        // Deltas
         deltaX = 0,
         deltaY = 0,
+        deltaH = 0,
         
-      //fudge factors
-        ffFa = 0.00009272, ffFb = 0.9221, ffFc = 0,
-        ffBa = -0.0001061, ffBb = 0.9187, ffBc = 0,
-        ffLa = 0, ffLb = 1, ffLc = 0,
-        ffRa = 0, ffRb = 1, ffRc = 0
+        //fudge factors
+        ffFa = 0.9046, ffFb = 1.0060,
+        ffBa = 0.8952, ffBb = 1.0090,
+        ffLa = 2.0240, ffLb = 0.8703,
+        ffRa = 1.6260, ffRb = 0.9174
     ;
     
     @Override
     public void runOpMode() throws InterruptedException {
       
-      // Setup Robot Position
-        Pose2d beginPose = new Pose2d(startX, startY, 0);
+        // Setup Robot Position
+        Pose2d beginPose = new Pose2d(startX, startY, startH);
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
         
         waitForStart();
         
-      // Create & Run Actions
+        // Create & Run Actions
         Actions.runBlocking(drive.actionBuilder(beginPose)
-          // Move to Chamber
-            .strafeTo(new Vector2d(
-                (deltaX>=0)?((ffFa*deltaX*deltaX)+(ffFb*deltaX)+(ffFc)):((ffBa*deltaX*deltaX)+(ffBb*deltaX)+(ffBc)),
-                (deltaY>=0)?((ffLa*deltaY*deltaY)+(ffLb*deltaY)+(ffLc)):((ffRa*deltaY*deltaY)+(ffRb*deltaY)+(ffRc))
-            ))
+            // Move to Delta Position
+            .strafeToLinearHeading(
+                new Vector2d(
+                    (deltaX>=0)?(ffFa*Math.pow(deltaY,ffFb)):(-ffBa*Math.pow(-deltaY,ffBb)),
+                    (deltaY>=0)?(ffLa*Math.pow(deltaY,ffLb)):(-ffRa*Math.pow(-deltaY,ffRb))
+                ),  (deltaH)
+            )
             
-          // Begin Loop
+            // Begin Loop
             .build())
         ;
     }
