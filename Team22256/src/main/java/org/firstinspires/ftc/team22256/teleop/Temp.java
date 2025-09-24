ae009e35fa7d4ed00ae872de2edc4db3ac947932
Armani Melo <Explodinglego3@gmail.com>
2025-09-23 17:56:22 -0700

    Temporary file for testing prototypes

---
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/teleop/Temp.java
@@ -0,0 +1,49 @@
+
+package org.firstinspires.ftc.team22256.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+
+public class Temp extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor one;
+    private DcMotor two;
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        one  = hardwareMap.get(DcMotor.class, "one");
+        two  = hardwareMap.get(DcMotor.class, "two");
+
+        two.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            if (gamepad1.a){
+                two.setPower(1);
+                one.setPower(1);
+            } else if(gamepad1.b){
+                two.setPower(-1);
+                one.setPower(-1);
+            } else {
+                two.setPower(0);
+                one.setPower(0);
+            }
+        }
+    }
+}
