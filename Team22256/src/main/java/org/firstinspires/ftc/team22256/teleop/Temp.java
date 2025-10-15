121fbd889db14a80a79fc3453e8074212e6a77c6
Armani Melo <Explodinglego3@gmail.com>
2025-10-14 18:02:37 -0700

    Added drivetrain

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/teleop/Temp.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/teleop/Temp.java
@@ -16,6 +16,7 @@ public class Temp extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor one;
     private DcMotor two;
+    private DcMotor frontRight,backRight,frontLeft,backLeft;
     @Override
     public void runOpMode() {
 
@@ -24,7 +25,14 @@ public class Temp extends LinearOpMode {
         one  = hardwareMap.get(DcMotor.class, "one");
         two  = hardwareMap.get(DcMotor.class, "two");
 
+        frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
+
         two.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         // Wait for the game to start (driver presses START)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -34,6 +42,7 @@ public class Temp extends LinearOpMode {
         waitForStart();
         runtime.reset();
         double power = 0;
+        double changePower = 1.25;
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
@@ -48,6 +57,29 @@ public class Temp extends LinearOpMode {
                 two.setPower(0);
                 one.setPower(0);
             }
+
+
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            telemetry.addData("left stick x", gamepad1.left_stick_x);
+            telemetry.addData("left stick y", gamepad1.left_stick_y);
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftPower = frontLeftPower / changePower;
+            backLeftPower = backLeftPower / changePower;
+            frontRightPower = frontRightPower / changePower;
+            backRightPower = backRightPower / changePower;
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
 
 
 
