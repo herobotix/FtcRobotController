bee38ba3d3ce69d15bcf70f8b716b15e8828e138
Armani Melo <Explodinglego3@gmail.com>
2025-10-20 15:47:04 -0700

    Added robot class

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/common/Robot.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/common/Robot.java
@@ -1,6 +1,20 @@
 package org.firstinspires.ftc.team22256.common;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Robot {
+    private DcMotor frontRight,backRight,frontLeft,backLeft;
+
+    public Robot(HardwareMap hardwareMap){
+        frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
+
+    }
+
+
 
 
 
