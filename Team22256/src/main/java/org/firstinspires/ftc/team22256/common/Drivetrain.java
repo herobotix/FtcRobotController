341319c627b2e3cec8612fefdf8b5167a7bc7e93
Armani Melo <Explodinglego3@gmail.com>
2025-10-22 15:18:36 -0700

    Upgraded Drivetrain to include power control and deadzone control to prevent stick drift

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/common/Drivetrain.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/common/Drivetrain.java
@@ -20,15 +20,29 @@ public class Drivetrain{
     }
 
     public void UpdateTeleOpDrivetrain(Gamepad gamepad1){
-
         double changePower = 1.25;
+
+        if (gamepad1.right_bumper) {
+            changePower = 1.0; // Full speed
+        } else if (gamepad1.left_bumper) {
+            changePower = 1.5; // Slower
+        } else {
+            changePower = 1.25; // Default
+        }
+
+
         double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
         double rx = gamepad1.right_stick_x;
 
+        if (Math.abs(x) < 0.05) x = 0; //Deadzone to handle stick drift
+        if (Math.abs(y) < 0.05) y = 0;
+        if (Math.abs(rx) < 0.05) rx = 0;
+
         telemetry.addData("left stick x", gamepad1.left_stick_x);
         telemetry.addData("left stick y", gamepad1.left_stick_y);
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1); // Power normalizer
         double frontLeftPower = (y + x + rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
         double frontRightPower = (y - x - rx) / denominator;
@@ -45,8 +59,5 @@ public class Drivetrain{
         backRight.setPower(backRightPower);
 
     }
-
-
-
 
 }
