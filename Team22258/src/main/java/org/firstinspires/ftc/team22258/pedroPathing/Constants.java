3a104f7be82b969c7ca26baf7f078c19076653c5
Lucas-183119 <lucas.183119@hastudents.us>
2025-12-03 17:49:24 -0700

    Began setting up Pedro Pathing.

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/pedroPathing/Constants.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/pedroPathing/Constants.java
@@ -3,17 +3,49 @@ package org.firstinspires.ftc.team22258.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+      .mass(8.355)
+    ;
+    
+    public static MecanumConstants driveConstants = new MecanumConstants()
+      .maxPower(1)
+      .leftFrontMotorName("FLMotor")
+      .rightFrontMotorName("FRMotor")
+      .leftRearMotorName("BLMotor")
+      .rightRearMotorName("BRMotor")
+      .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+      .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+      .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+      .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+    ;
+    
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+      .forwardPodY(-3)
+      .strafePodX(-4.125)
+      .distanceUnit(DistanceUnit.INCH)
+      .hardwareMapName("pinpoint")
+      .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+      .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+      .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+    ;
+    
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
-                .pathConstraints(pathConstraints)
-                .build();
+            .pinpointLocalizer(localizerConstants)
+            .pathConstraints(pathConstraints)
+            .mecanumDrivetrain(driveConstants)
+            .build();
     }
 }
