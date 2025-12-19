ea5ba6149b84987829c15b1f6d74babbea09a672
Lucas-183119 <lucas.183119@hastudents.us>
2025-12-19 15:02:47 -0700

    Update Auto to Version 1.2.0
    Rewrote Everything, copying from the PedroPathing docs.

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/teamcode/opmode_Auto.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/teamcode/opmode_Auto.java
@@ -1,298 +1,254 @@
 package org.firstinspires.ftc.team22258.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import org.firstinspires.ftc.team22258.pedroPathing.Constants;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
-@Autonomous(name = "Opmode (Auto) [1.1.7]")
+
+@Autonomous(name = "Opmode (Auto) [1.2.0]", group = "Autonomous")
+@Configurable
 public class opmode_Auto extends LinearOpMode {
-
-  //private DcMotor Arm;
-  private DcMotor FLM;
-  private DcMotor FRM;
-  private DcMotor BLM;
-  private DcMotor BRM;
-  //private Servo LClaw;
-  //private Servo RClaw;
-
-  public final double Pi = 3.1415926535;
-  public final double sqrt8 = 2.8284271247;
   
-  public final double wD = 2.99 /* +/-0.004 */;
-  public final double pR = 10.5;
+  public static class Paths {
+    
+    public PathChain SetupOuttake;
+    public PathChain FirstAlign;
+    public PathChain FirstIntake;
+    public PathChain FirstOuttake;
+    public PathChain SecondAlign;
+    public PathChain SecondIntake;
+    public PathChain SecondOuttake;
+    public PathChain ThirdAlign;
+    public PathChain ThirdIntake;
+    public PathChain ThirdOuttake;
+    public PathChain EndAlign;
+    
+    public Paths(Follower follower) {
+      SetupOuttake = follower
+        .pathBuilder()
+        .addPath(
+          new BezierLine(new Pose(56.500, 8.500), new Pose(56.500, 13.000))
+        )
+        .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(54.5))
+        .build();
+      
+      FirstAlign = follower
+        .pathBuilder()
+        .addPath(
+          new BezierLine(new Pose(56.500, 13.000), new Pose(56.500, 36.000))
+        )
+        .setLinearHeadingInterpolation(
+          Math.toRadians(54.5),
+          Math.toRadians(180)
+        )
+        .build();
+      
+      FirstIntake = follower
+        .pathBuilder()
+        .addPath(
+          new BezierLine(new Pose(56.500, 36.000), new Pose(20.000, 36.000))
+        )
+        .setTangentHeadingInterpolation()
+        .build();
+      
+      FirstOuttake = follower
+        .pathBuilder()
+        .addPath(
+          new BezierCurve(
+            new Pose(20.000, 36.000),
+            new Pose(56.500, 36.000),
+            new Pose(56.500, 13.000)
+          )
+        )
+        .setLinearHeadingInterpolation(
+          Math.toRadians(180),
+          Math.toRadians(54.5)
+        )
+        .build();
+      
+      SecondAlign = follower
+        .pathBuilder()
+        .addPath(
+          new BezierLine(new Pose(56.500, 13.000), new Pose(56.500, 60.000))
+        )
+        .setLinearHeadingInterpolation(
+          Math.toRadians(54.5),
+          Math.toRadians(180)
+        )
+        .build();
+      
+      SecondIntake = follower
+        .pathBuilder()
+        .addPath(
+          new BezierLine(new Pose(56.500, 60.000), new Pose(20.000, 60.000))
+        )
+        .setTangentHeadingInterpolation()
+        .build();
+      
+      SecondOuttake = follower
+        .pathBuilder()
+        .addPath(
+          new BezierCurve(
+            new Pose(20.000, 60.000),
+            new Pose(56.500, 60.000),
+            new Pose(56.500, 13.000)
+          )
+        )
+        .setLinearHeadingInterpolation(
+          Math.toRadians(180),
+          Math.toRadians(54.5)
+        )
+        .build();
+      
+      ThirdAlign = follower
+        .pathBuilder()
+        .addPath(
+          new BezierLine(new Pose(56.500, 13.000), new Pose(56.500, 84.000))
+        )
+        .setLinearHeadingInterpolation(
+          Math.toRadians(54.5),
+          Math.toRadians(180)
+        )
+        .build();
+      
+      ThirdIntake = follower
+        .pathBuilder()
+        .addPath(
+          new BezierLine(new Pose(56.500, 84.000), new Pose(20.000, 84.000))
+        )
+        .setTangentHeadingInterpolation()
+        .build();
+      
+      ThirdOuttake = follower
+        .pathBuilder()
+        .addPath(
+          new BezierCurve(
+            new Pose(20.000, 84.000),
+            new Pose(56.500, 84.000),
+            new Pose(56.500, 13.000)
+          )
+        )
+        .setLinearHeadingInterpolation(
+          Math.toRadians(180),
+          Math.toRadians(54.5)
+        )
+        .build();
+      
+      EndAlign = follower
+        .pathBuilder()
+        .addPath(
+          new BezierCurve(
+            new Pose(56.500, 13.000),
+            new Pose(56.500, 24.000),
+            new Pose(48.000, 24.000)
+          )
+        )
+        .setLinearHeadingInterpolation(Math.toRadians(54.5), Math.toRadians(90))
+        .build();
+    }
+  }
   
-  public final double TicksPerRev = (28 * 2.89 * 5.23); //Ticks per Revolution of the Wheel
-  public final double TicksPerInch = (TicksPerRev /(Pi*wD)); //Ticks per Inch Traveled
+  private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+  public Follower follower; // Pedro Pathing follower instance
+  private int pathState; // Current autonomous path state (state machine)
+  private Paths paths; // Paths defined in the Paths class
   
-  public final double RotCorrection = 1;
-  public final double RotPerRev = ((wD)/(pR*sqrt8)) * RotCorrection;
-  public final double TicksPerRot = TicksPerRev / RotPerRev; //Ticks per Rotation of the Robot
-  
-  double FLMP;
-  double FRMP;
-  double BLMP;
-  double BRMP;
-  
-  double FLMD;
-  double FRMD;
-  double BLMD;
-  double BRMD;
-  
-  /*double Lynn;
-  double Lake;
-  double Rick;
-  double FArmInput;
-  int Clawn = 0;
-  double Naptime;*/
-  
-  double MPN;
-
   private IMU rIMU;
+  
+  private LIMELIGHT Limelight;
   
   @Override
   public void runOpMode() {
-    //Arm = hardwareMap.get(DcMotor.class, "Arm");
-    FLM = hardwareMap.get(DcMotor.class, "FLMotor");
-    FRM = hardwareMap.get(DcMotor.class, "FRMotor");
-    BLM = hardwareMap.get(DcMotor.class, "BLMotor");
-    BRM = hardwareMap.get(DcMotor.class, "BRMotor");
-    //LClaw = hardwareMap.get(Servo.class, "LClaw");
-    //RClaw = hardwareMap.get(Servo.class, "RClaw");
-
-      F_IMU();
-      F_Startup();
-      waitForStart();
-    if (opModeIsActive()) {
-      F_Run();
-    }
+    //Begin
+    
+    //Init & Wait
+    Fn_Init();
+    waitForStart();
+    Fn_OnStart();
+    
+    //Run Opmode
+    while (opModeIsActive()) { Fn_loop(); }
+    Fn_OnStop();
+    
   }
-
-/**
-   * Describe this function...
-   */
-  private void F_IMU() {
-    // Retrieve the IMU from the hardware map
-    rIMU = hardwareMap.get(IMU.class, "rIMU");
-    // Adjust the orientation parameters to match your robot
-    IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-            RevHubOrientationOnRobot.LogoFacingDirection.UP,
-            RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
-    // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-    rIMU.initialize(parameters);
-  }
-
-  /**
-   * Describe this function...
-   */
-  private void F_Startup() {
-    //Set Motor Directions
-      FLM.setDirection(DcMotor.Direction.FORWARD);
-      FRM.setDirection(DcMotor.Direction.REVERSE);
-      BLM.setDirection(DcMotor.Direction.REVERSE);
-      BRM.setDirection(DcMotor.Direction.REVERSE);
+  
+  private void Fn_Init() {
+    // Initialization Code
     
-    //Stop, Reset, & Run using Motor Encoders
-      FLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      FLM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      FRM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      FRM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      BLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      BLM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      BLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      BRM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    // ???
+      panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+      
+      follower = Constants.createFollower(hardwareMap);
+      follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
+      
+      paths = new Paths(follower); // Build paths
+      
+      panelsTelemetry.debug("Status", "Initialized");
+      panelsTelemetry.update(telemetry);
     
-    //Stop Arm w/ input of 0
-      //Arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-  }
-
-  /**
-   * Describe this function...
-   */
-  private void F_Move(
-    double dstHead, double powHead,
-    double dstSide, double powSide,
-    double dstTurn, double powTurn
-  ) {
+    // Map Hardware
+      rIMU = hardwareMap.get(IMU.class, "rIMU");
     
-    //Reset Encoders
-      FLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      FRM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      BLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      BRM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    
-    // Drive & Strafe Power
-      FLMP = powHead + powSide - powTurn;
-      FRMP = powHead - powSide + powTurn;
-      BLMP = powHead - powSide - powTurn;
-      BRMP = powHead + powSide + powTurn;
-    
-    //telemetry
-      double FLMPo = FLMP;
-      double FRMPo = FRMP;
-      double BLMPo = BLMP;
-      double BRMPo = BRMP;
-    
-    // Power Control
-      MPN = (
-        1/Math.max( 
-          Math.max( Math.abs(FLMP), Math.abs(FRMP) ), 
-          Math.max( Math.abs(BLMP), Math.abs(BRMP) ) 
-        )
+    // Set Robot Orientation (IMU)
+      IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+        RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD)
       );
-      if (MPN < 1){
-        BRMP *= MPN;
-        BLMP *= MPN;
-        FRMP *= MPN;
-        FLMP *= MPN;
-      }
+      rIMU.initialize(parameters);
     
-    // Drive & Strafe Distance
-      FLMD = (dstHead + dstSide) * TicksPerInch - dstTurn * TicksPerRot;
-      FRMD = (dstHead - dstSide) * TicksPerInch + dstTurn * TicksPerRot;
-      BLMD = (dstHead - dstSide) * TicksPerInch - dstTurn * TicksPerRot;
-      BRMD = (dstHead + dstSide) * TicksPerInch + dstTurn * TicksPerRot;
+    // Limelight
+      Limelight = new LIMELIGHT();
+      Limelight.Init(hardwareMap);
     
-    //telemetry
-      telemetry.addData("MPN", MPN);
-      telemetry.addData("FLMP", FLMP);
-      telemetry.addData("FRMP", FRMP);
-      telemetry.addData("BLMP", BLMP);
-      telemetry.addData("BRMP", BRMP);
-      telemetry.addData("FLMD", FLMD);
-      telemetry.addData("FLMD", FRMD);
-      telemetry.addData("BLMD", BLMD);
-      telemetry.addData("BRMD", BRMD);
-      telemetry.addData("FLMP OLD", FLMPo);
-      telemetry.addData("FRMP OLD", FRMPo);
-      telemetry.addData("BLMP OLD", BLMPo);
-      telemetry.addData("BRMP OLD", BRMPo);
-      telemetry.update();
-    
-    // Set target position
-      FLM.setTargetPosition((int)FLMD);
-      FRM.setTargetPosition((int)FRMD);
-      BLM.setTargetPosition((int)BLMD);
-      BRM.setTargetPosition((int)BRMD);
-    
-    // Set Motors to "BUSY" until target distance reached.
-      FLM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      FRM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      BLM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      BRM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    
-    //Set Motor Power
-      FLM.setPower(-FLMP);
-      FRM.setPower(-FRMP);
-      BLM.setPower(-BLMP);
-      BRM.setPower(-BRMP);
-    
-    while (opModeIsActive()&&(FLM.isBusy()||FRM.isBusy()||BLM.isBusy()||BRM.isBusy())) {
-        if (!FLM.isBusy()&&!(FLM.getPower()==0)) {FLM.setPower(0);}
-        if (!FRM.isBusy()&&!(FRM.getPower()==0)) {FRM.setPower(0);}
-        if (!BLM.isBusy()&&!(BLM.getPower()==0)) {BLM.setPower(0);}
-        if (!BRM.isBusy()&&!(BRM.getPower()==0)) {BRM.setPower(0);}
-        //telemetry
-          telemetry.addData("FLMTp", FLM.getCurrentPosition());
-          telemetry.addData("FRMTp", FRM.getCurrentPosition());
-          telemetry.addData("BLMTp", BLM.getCurrentPosition());
-          telemetry.addData("BRMTp", BRM.getCurrentPosition());
-          telemetry.addData("MPN", MPN);
-          telemetry.addData("FLMb", FLM.isBusy());
-          telemetry.addData("FRMb", FRM.isBusy());
-          telemetry.addData("BLMb", BLM.isBusy());
-          telemetry.addData("BRMb", BRM.isBusy());
-          telemetry.addData("FLMP", FLMP);
-          telemetry.addData("FRMP", FRMP);
-          telemetry.addData("BLMP", BLMP);
-          telemetry.addData("BRMP", BRMP);
-          telemetry.addData("FLMD", FLMD);
-          telemetry.addData("FRMD", FRMD);
-          telemetry.addData("BLMD", BLMD);
-          telemetry.addData("BRMD", BRMD);
-          telemetry.addData("FLMDt", (int)FLMD);
-          telemetry.addData("FRMDt", (int)FRMD);
-          telemetry.addData("BLMDt", (int)BLMD);
-          telemetry.addData("BRMDt", (int)BRMD);
-          telemetry.addData("FLMP OLD", FLMPo);
-          telemetry.addData("FRMP OLD", FRMPo);
-          telemetry.addData("BLMP OLD", BLMPo);
-          telemetry.addData("BRMP OLD", BRMPo);
-          telemetry.update();
-        
-      }
-    
-    // Stop Motors not caught in while loop.
-      FLM.setPower(0);
-      FRM.setPower(0);
-      BLM.setPower(0);
-      BRM.setPower(0);
-    
-    // Return motors to using the encoder.
-      FLM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      FRM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      BLM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      BRM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
   }
   
-/*
-  private void F_Clawrm(int Clawn) {
-    LClaw.setPosition(0 == Clawn ? 0.75 : 0.25);
-    RClaw.setPosition(1 == Clawn ? 0.75 : 0.25);
+  private void Fn_OnStart() {
+    // Run On START
     
-    //telemetry
-      telemetry.addData("Clawn", Clawn);
-      telemetry.addData("LClawPos", LClaw.getPosition());
-      telemetry.addData("RClawPos", RClaw.getPosition());
-      telemetry.update();
-    
+    // Limelight
+      Limelight.Start();
+      
   }
-*/
-
-/*
-  private void F_NewArmon(double FArmInput) {
-    Arm.setPower(FArmInput);
-    
-    //telemetry
-      telemetry.addData("FArmInput", FArmInput);
-      telemetry.update();
-    
-  }
-*/
   
-  /**
-   * Describe this function...
-   */
-//  private void F_Pause(double Naptime) {
-//      sleep((long)(Naptime*1000));
-//  }
-  
-   /* //selection
-    F_Move(
-      0.0, 0.0,
-      0.0, 0.0,
-      0.0, 0.0
-    );
-    F_NewArmon(0.0);
-    F_Clawrm(0);
-    F_Pause(0.0);
+  public void Fn_loop() {
+    follower.update(); // Update Pedro Pathing
+    pathState = autonomousPathUpdate(); // Update autonomous state machine
     
-   /**
-    * Describe this function...
-    */
-  private void F_Run() {
-    F_Move(
-      -12.0, 0.5,
-      0.0, 0.0,
-      0.0, 0.0
-    );
-//    F_Move(
-//      0.0, 0.0,
-//      0.0, 0.0,
-//      1.0, 0.5
-//    );
+    // Log values to Panels and Driver Station
+    panelsTelemetry.debug("Path State", pathState);
+    panelsTelemetry.debug("X", follower.getPose().getX());
+    panelsTelemetry.debug("Y", follower.getPose().getY());
+    panelsTelemetry.debug("Heading", follower.getPose().getHeading());
+    panelsTelemetry.update(telemetry);
   }
+  
+  public int autonomousPathUpdate() {
+    // Add your state machine Here
+    // Access paths with paths.pathName
+    // Refer to the Pedro Pathing Docs (Auto Example) for an example state machine
+    return pathState;
+  }
+  
+  private void Fn_OnStop() {
+    //Run On STOP
+    
+    //Limelight
+    Limelight.Stop();
+    
+  }
+  
 }
