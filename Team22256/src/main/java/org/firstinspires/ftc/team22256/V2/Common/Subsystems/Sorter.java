29f8bc0a04102b44ea4504689935713209357248
armani-183198 <armani.183198@hastudents.us>
2025-12-28 22:37:36 -0700

    Sorter

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.team22256.V2.Common.Subsystems;
 
-public class Sorter {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+public class  Sorter {
+    private Servo kicker1, kicker2, kicker3;
+    private ColorSensor sensor1, sensor2, sensor3;
+
+    public enum sorterState {
+        KICK,
+        SENSE,
+    }
+    private sorterState sorterState;
+
+    public Sorter(HardwareMap hardwareMap){
+        kicker1 = hardwareMap.get(Servo.class,"sorter");
+    }
+
+
+
 }
--- Team22256/src/main/java/org/firstinspires/ftc/team22256/legacy/2025-2026/V1/common/Robot.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/legacy/2025-2026/V1/common/Robot.java
@@ -5,17 +5,17 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Robot {
     private DcMotor frontRight,backRight,frontLeft,backLeft;
-    Drivetrain drivetrain;
-    Intake intake;
-    Turret turret;
+    //Drivetrain drivetrain;
+    //Intake intake;
+    //Turret turret;
     public Robot(HardwareMap hardwareMap){
-        drivetrain = new Drivetrain(hardwareMap);
+      //  drivetrain = new Drivetrain(hardwareMap);
         frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
         backRight  = hardwareMap.get(DcMotor.class, "backRight");
         frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
         backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
-        intake = new Intake();
-        turret = new Turret();
+       // intake = new Intake();
+        //turret = new Turret();
 
     }
 
