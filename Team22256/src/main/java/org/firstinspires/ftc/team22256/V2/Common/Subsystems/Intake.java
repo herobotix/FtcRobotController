03ee959b871cb3c1a732694bbbfe2272b2e7916a
armani-183198 <armani.183198@hastudents.us>
2026-01-09 19:04:05 -0700

    Updated Intake Subsystem
    Added new commands using NextFTC

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Intake.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Intake.java
@@ -11,47 +11,56 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.powerable.SetPower;
 
 
 public class Intake implements Subsystem {
     public static final Intake INSTANCE = new Intake();
     private Intake(){ }
-    private DcMotor intake;
+    private MotorEx intake = new MotorEx("intake");
 
-    public enum IntakeState{
-        INTAKING,
-        OUTAKING,
-        STOPPED;
+    private  double intakePower = 1;
+    private  double outtakePower = -1;
+    private final int stopPower = 0;
+
+    public void setIntakePower(double intakePower) {
+        this.intakePower = intakePower;
+    }
+    public void setOuttakePower(double outtakePower){
+        this.outtakePower = outtakePower;
+    }
+
+    public Command intaking = new LambdaCommand()
+            .setStart(() -> intake.setPower(intakePower))
+            .setIsDone(() -> true)
+            .requires(this);
+    public Command outtaking = new LambdaCommand()
+            .setStart(() -> intake.setPower(outtakePower))
+            .setIsDone(() -> true);
+
+    public Command stop = new LambdaCommand()
+            .setStart(() -> intake.setPower(stopPower))
+            .setIsDone(() -> true);
+    public double getIntakePower() {
+        return intakePower;
+    }
+
+    public double getOuttakePower() {
+        return outtakePower;
     }
 
 
-     IntakeState intakeState = IntakeState.STOPPED;
 
 
-    public Intake(HardwareMap hardwareMap){
-        intake = hardwareMap.get(DcMotor.class,"intake");
+
     }
 
-    public IntakeState getIntakeState(){
-        return intakeState;
-    }
-    public void setIntakeState(IntakeState intakeState){
-        this.intakeState = intakeState;
-    }
-    public void updateIntake(){
-        switch (intakeState){
-            case INTAKING:
-                intake.setPower(1);
-                break;
-            case OUTAKING:
-                intake.setPower(-1);
-                break;
-            case STOPPED:
-                intake.setPower(0);
-                break;
-            default:
-                intakeState = IntakeState.STOPPED;
-        }
-    }
-}
+
+
+
+
+
