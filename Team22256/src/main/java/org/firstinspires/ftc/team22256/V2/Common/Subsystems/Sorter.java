79dc233846f4cfe6024466c34634be86135402e1
armani-183198 <armani.183198@hastudents.us>
2026-01-17 16:15:36 -0700

    Updated Sorter Class

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
@@ -12,21 +12,84 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import org.firstinspires.ftc.team22256.V2.Common.Subsystems.NormColorSensor;
 
-public class  Sorter {
-    private Servo kicker1, kicker2, kicker3;
-    private ColorSensor sensor1, sensor2, sensor3;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.LambdaCommand;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.positionable.SetPosition;
 
-    public enum sorterState {
-        KICK,
-        SENSE,
+
+public class Sorter implements Subsystem {
+    public static final Sorter INSTANCE = new Sorter();
+    private Sorter(){
+        LKSL = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
+        LKSR = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
+        RKSL = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
+        RKSR = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
+        BKSL = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
+        BKSR = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
     }
-    private sorterState sorterState;
 
-    public Sorter(HardwareMap hardwareMap){
-        kicker1 = hardwareMap.get(Servo.class,"sorter");
+    private ServoEx LK = new ServoEx("l-kicker");
+    private ServoEx RK = new ServoEx("r-kicker");
+    private ServoEx BK = new ServoEx("b-kicker");
+    private NormColorSensor LKSL;//left kicker sensor left
+    private NormColorSensor LKSR;//left kicker sensor right
+    private NormColorSensor RKSL;//right kicker sensor left
+    private NormColorSensor RKSR;//right kicker sensor right
+    private NormColorSensor BKSL;//back kicker sensor left
+    private NormColorSensor BKSR;//back kicker sensor right
+    private final double LKDown = 0;
+    private final double LKUp = 0;
+    private final double RKDown = 0;
+    private final double RKUp = 0;
+    private final double BKDown = 0;
+    private final double BKUp = 0;
+    int LKSLC;//left kicker sensor left color
+    int LKSRC;//left kicker sensor right color
+    int RKSLC;//right kicker sensor left color
+    int RKSRC;//right kicker sensor right color
+    int BKSLC;//back kicker sensor left color
+    int BKSRC;//back kicker sensor right color
+
+
+public Command LK_Up = new LambdaCommand()
+        .setStart(() -> LK.setPosition(LKUp))
+        .setIsDone(() -> true)
+        .requires(this);
+
+    public Command LK_Down = new LambdaCommand()
+            .setStart(() -> LK.setPosition(LKDown))
+            .setIsDone(() -> true)
+            .requires(this);
+
+    public Command RK_Up = new LambdaCommand()
+            .setStart(() -> RK.setPosition(RKUp))
+            .setIsDone(() -> true)
+            .requires(this);
+    public Command RK_Down = new LambdaCommand()
+            .setStart(() -> RK.setPosition(RKDown))
+            .setIsDone(() -> true)
+            .requires(this);
+    public Command BK_Up = new LambdaCommand()
+            .setStart(() -> BK.setPosition(BKUp))
+            .setIsDone(() -> true)
+            .requires(this);
+    public Command BK_Down = new LambdaCommand()
+            .setStart(() -> BK.setPosition(BKDown))
+            .setIsDone(() -> true)
+            .requires(this);
+
+    @Override
+    public void periodic() {
+        LKSLC = LKSL.getDetectedColor();
+        LKSRC = LKSR.getDetectedColor();
+        RKSLC = RKSL.getDetectedColor();
+        RKSRC = RKSR.getDetectedColor();
+        BKSLC = BKSL.getDetectedColor();
+        BKSRC = BKSR.getDetectedColor();
     }
-
-
-
 }
