62f8444a0bf27a87fdf22d70178fda14308daf7a
armani-183198 <armani.183198@hastudents.us>
2026-01-21 22:51:46 -0700

    Updated methods in sorter class

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
@@ -15,11 +15,14 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import org.firstinspires.ftc.team22256.V2.Common.Subsystems.NormColorSensor;
 
 import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.impl.ServoEx;
 import dev.nextftc.hardware.positionable.SetPosition;
+import dev.nextftc.core.commands.groups.SequentialGroup;
 
 
 public class Sorter implements Subsystem {
@@ -32,10 +35,10 @@ public class Sorter implements Subsystem {
         BKSL = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
         BKSR = new NormColorSensor(ActiveOpMode.hardwareMap(),"String");
     }
-
-    private ServoEx LK = new ServoEx("l-kicker");
-    private ServoEx RK = new ServoEx("r-kicker");
-    private ServoEx BK = new ServoEx("b-kicker");
+    int[] colorPositions = {0,0,0,0,0,0};
+    private static ServoEx LK = new ServoEx("l-kicker");
+    private static ServoEx RK = new ServoEx("r-kicker");
+    private static ServoEx BK = new ServoEx("b-kicker");
     private NormColorSensor LKSL;//left kicker sensor left
     private NormColorSensor LKSR;//left kicker sensor right
     private NormColorSensor RKSL;//right kicker sensor left
@@ -43,53 +46,40 @@ public class Sorter implements Subsystem {
     private NormColorSensor BKSL;//back kicker sensor left
     private NormColorSensor BKSR;//back kicker sensor right
     private final double LKDown = 0;
-    private final double LKUp = 0;
-    private final double RKDown = 0;
-    private final double RKUp = 0;
-    private final double BKDown = 0;
-    private final double BKUp = 0;
+    private static final double LKUp = 0;
+    private static final double RKDown = 0;
+    private static final double RKUp = 0;
+    private static final double BKDown = 0;
+    private static final double BKUp = 0;
     int LKSLC;//left kicker sensor left color
     int LKSRC;//left kicker sensor right color
     int RKSLC;//right kicker sensor left color
     int RKSRC;//right kicker sensor right color
     int BKSLC;//back kicker sensor left color
     int BKSRC;//back kicker sensor right color
-
-
-public Command LK_Up = new LambdaCommand()
-        .setStart(() -> LK.setPosition(LKUp))
-        .setIsDone(() -> true)
-        .requires(this);
-
-    public Command LK_Down = new LambdaCommand()
-            .setStart(() -> LK.setPosition(LKDown))
-            .setIsDone(() -> true)
-            .requires(this);
-
-    public Command RK_Up = new LambdaCommand()
-            .setStart(() -> RK.setPosition(RKUp))
-            .setIsDone(() -> true)
-            .requires(this);
-    public Command RK_Down = new LambdaCommand()
-            .setStart(() -> RK.setPosition(RKDown))
-            .setIsDone(() -> true)
-            .requires(this);
-    public Command BK_Up = new LambdaCommand()
-            .setStart(() -> BK.setPosition(BKUp))
-            .setIsDone(() -> true)
-            .requires(this);
-    public Command BK_Down = new LambdaCommand()
-            .setStart(() -> BK.setPosition(BKDown))
-            .setIsDone(() -> true)
-            .requires(this);
+    public static Command LK_Up = new InstantCommand(() -> LK.setPosition(LKUp));
+    public static Command LK_Down = new InstantCommand(() -> LK.setPosition(LKUp));
+    public static Command RK_Up = new InstantCommand(() -> RK.setPosition(RKUp));
+    public static Command RK_Down = new InstantCommand(() -> RK.setPosition(RKDown));
+    public static Command BK_Up = new InstantCommand(() -> BK.setPosition(BKUp));
+    public static Command BK_Down = new InstantCommand(() -> BK.setPosition(BKDown));
+    public static Command LK_UpDown(){
+        return new SequentialGroup(LK_Up,new Delay(0.2), LK_Down);
+    }
+    public static Command RK_UpDown(){
+        return new SequentialGroup(RK_Up,new Delay(0.2), RK_Down);
+    }
+    public static Command BK_UpDown(){
+        return new SequentialGroup(BK_Up,new Delay(0.2), BK_Down);
+    }
 
     @Override
     public void periodic() {
-        LKSLC = LKSL.getDetectedColor();
-        LKSRC = LKSR.getDetectedColor();
-        RKSLC = RKSL.getDetectedColor();
-        RKSRC = RKSR.getDetectedColor();
-        BKSLC = BKSL.getDetectedColor();
-        BKSRC = BKSR.getDetectedColor();
+        colorPositions[0] = LKSL.getDetectedColor();
+        colorPositions[1] = LKSR.getDetectedColor();
+        colorPositions[2] = RKSL.getDetectedColor();
+        colorPositions[3] = RKSR.getDetectedColor();
+        colorPositions[4] = BKSL.getDetectedColor();
+        colorPositions[5] = BKSR.getDetectedColor();
     }
 }
