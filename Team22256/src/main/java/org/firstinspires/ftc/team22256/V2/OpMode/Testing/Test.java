d2e93420474c63dbd0551cd03de0f271a0bf938f
armani-183198 <armani.183198@hastudents.us>
2026-01-26 16:25:45 -0700

    Test Opmode refitted to be main tele-op

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/OpMode/Testing/Test.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/OpMode/Testing/Test.java
@@ -2,10 +2,15 @@ package org.firstinspires.ftc.team22256.V2.OpMode.Testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.team22256.V2.Common.Commands.ShootMotif;
 import org.firstinspires.ftc.team22256.V2.Common.Subsystems.Intake;
+import org.firstinspires.ftc.team22256.V2.Common.Subsystems.Shooter;
 import org.firstinspires.ftc.team22256.V2.Common.Subsystems.Sorter;
+import org.firstinspires.ftc.team22256.V2.Common.Subsystems.Turret;
 
 import dev.nextftc.bindings.BindingManager;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.Gamepads;
@@ -29,7 +34,7 @@ public class Test extends NextFTCOpMode {
     public Test(){
         addComponents(
                 BindingsComponent.INSTANCE,
-                new SubsystemComponent(Intake.INSTANCE, Sorter.INSTANCE)
+                new SubsystemComponent(Intake.INSTANCE, Sorter.INSTANCE, Shooter.INSTANCE, Turret.INSTANCE)
         );
     }
 
@@ -40,39 +45,57 @@ public class Test extends NextFTCOpMode {
                 frontRight,
                 backLeft,
                 backRight,
-                Gamepads.gamepad1().leftStickY().negate(),
-                Gamepads.gamepad1().leftStickX(),
-                Gamepads.gamepad1().rightStickX()
+                Gamepads.gamepad2().leftStickY().negate(),
+                Gamepads.gamepad2().leftStickX(),
+                Gamepads.gamepad2().rightStickX()
         );
         driverControlled.schedule();
+
+        ShootMotif shootMotif = new ShootMotif();
 
         Gamepads.gamepad1().a()
                 .toggleOnBecomesTrue()
                 .whenBecomesTrue(Intake.changeIntakeMode(Intake.Mode.INTAKING))
                 .whenBecomesFalse(Intake.changeIntakeMode(Intake.Mode.PAUSED)
         );
-
+/*
         Gamepads.gamepad1().b()
-                .toggleOnBecomesTrue()
+
                 .whenBecomesTrue(Sorter.RK_Up)
                 .whenBecomesFalse(Sorter.RK_Down
                 );
+*/
+        Gamepads.gamepad1().b()
 
+                .whenBecomesTrue(new SequentialGroup(
+                        Sorter.RK_UpDown(),
+                        new Delay(0.2),
+                        Sorter.LK_UpDown(),
+                        new Delay(0.2),
+                        Sorter.BK_UpDown()
+                        )
+
+
+                );
         Gamepads.gamepad1().x()
-                .toggleOnBecomesTrue()
+
                 .whenBecomesTrue(Sorter.LK_Up)
                 .whenBecomesFalse(Sorter.LK_Down
                 );
 
         Gamepads.gamepad1().y()
-                .toggleOnBecomesTrue()
                 .whenBecomesTrue(Sorter.BK_Up)
                 .whenBecomesFalse(Sorter.BK_Down
                 );
+        Gamepads.gamepad1().dpadRight().whenBecomesTrue(Shooter.farTriangle);
+        Gamepads.gamepad1().dpadLeft().whenBecomesTrue(Shooter.closeTriangle);
+        Gamepads.gamepad1().dpadUp().whenBecomesTrue(Intake.changeIntakeMode(Intake.Mode.OUTAKING));
 
     }
     @Override
     public void onUpdate(){
-
+        telemetry.addData("vel",Shooter.getLeftVelocity());
+        telemetry.addData("vel2", Shooter.getRightVelocity());
+        telemetry.update();
     }
 }
