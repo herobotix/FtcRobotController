c264cb63c00aa22da9797c6b579125f02aa0e132
armani-183198 <armani.183198@hastudents.us>
2026-01-26 16:29:09 -0700

    Experimental color sorting

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Sorter.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.team22256.V2.Common.Subsystems;
 
+import static org.firstinspires.ftc.team22256.V2.Common.Global.motif;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -13,6 +15,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import org.firstinspires.ftc.team22256.V2.Common.Subsystems.NormColorSensor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.Delay;
@@ -29,28 +34,28 @@ public class Sorter implements Subsystem {
     public static final Sorter INSTANCE = new Sorter();
     private Sorter(){
     }
-    int[] colorPositions = {0,0,0,0,0,0};
+
+
     private static ServoEx LK = new ServoEx("l-kicker");
     private static ServoEx RK = new ServoEx("r-kicker");
     private static ServoEx BK = new ServoEx("b-kicker");
-    private NormColorSensor LKSL;//left kicker sensor left
-    private NormColorSensor LKSR;//left kicker sensor right
-    private NormColorSensor RKSL;//right kicker sensor left
-    private NormColorSensor RKSR;//right kicker sensor right
-    private NormColorSensor BKSL;//back kicker sensor left
-    private NormColorSensor BKSR;//back kicker sensor right
+    //private NormColorSensor LKSL;//left kicker sensor left
+    //private NormColorSensor LKSR;//left kicker sensor right
+    //private NormColorSensor RKSL;//right kicker sensor left
+    //private NormColorSensor RKSR;//right kicker sensor right
+    //private NormColorSensor BKSL;//back kicker sensor left
+    //private NormColorSensor BKSR;//back kicker sensor right
+    private static final NormColorSensor[][] sensors = new NormColorSensor[3][2]; //3 ball slots;average the value from the 2 color sensor per spot
+    private static final ServoEx[] kickers = new ServoEx[3];// 1 kicker per ball slot
+    private static NormColorSensor.COLOR[] storedColors = new NormColorSensor.COLOR[3];//1 color per ball slot
+    public static final List<Integer> kickQueue = new ArrayList<>();
+    private int queueIndex = 0;
     private static final double LKDown = 0.025;
     private static final double LKUp = 0.4;
     private static final double RKDown = 0.95;
-    private static final double RKUp = 0.6;
-    private static final double BKDown = 1;
+    private static final double RKUp = 0.55;
+    private static final double BKDown = 0.66;
     private static final double BKUp = 0;
-    int LKSLC;//left kicker sensor left color
-    int LKSRC;//left kicker sensor right color
-    int RKSLC;//right kicker sensor left color
-    int RKSRC;//right kicker sensor right color
-    int BKSLC;//back kicker sensor left color
-    int BKSRC;//back kicker sensor right color
     public static Command LK_Up = new InstantCommand(() -> LK.setPosition(LKUp));
     public static Command LK_Down = new InstantCommand(() -> LK.setPosition(LKDown));
     public static Command RK_Up = new InstantCommand(() -> RK.setPosition(RKUp));
@@ -67,23 +72,76 @@ public class Sorter implements Subsystem {
         return new SequentialGroup(BK_Up,new Delay(0.2), BK_Down);
     }
 
+    public static NormColorSensor.COLOR updateSpotColor(int spot){
+        float normRed = (sensors[spot][0].getRed() + sensors[spot][1].getRed())/2;
+        float normGreen = (sensors[spot][0].getGreen() + sensors[spot][1].getGreen())/2;
+        float normBlue = (sensors[spot][0].getBlue() + sensors[spot][1].getBlue())/2;
+        if(normBlue > normGreen && normRed > (0.5 * normGreen) && normRed > 0.01){
+            return NormColorSensor.COLOR.PURPLE; //purple
+        } else if(normGreen > 0.03 && normGreen > (normRed + 0.01) && normGreen > (normBlue + 0.003)){
+            return NormColorSensor.COLOR.GREEN; //green
+        } else{
+            return NormColorSensor.COLOR.EMPTY;//none
+        }
+    }
+    public static void updateAllSpots(){
+        for(int i = 0; i < 3;i++){
+            storedColors[i] = updateSpotColor(i);
+        }
+    }
+
+    private static boolean motifMatches() {
+        for (int i = 0; i < 3; i++) {
+            if (storedColors[i] != motif[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void buildKickQueue() {
+        kickQueue.clear();
+
+        if (motifMatches()) {
+            // Motif order: slot 0 → 1 → 2
+            for (int i = 0; i < 3; i++) {
+                kickQueue.add(i);
+            }
+        } else {
+            // First shoot all PURPLES
+            for (int i = 0; i < 3; i++) {
+                if (storedColors[i] == NormColorSensor.COLOR.PURPLE) {
+                    kickQueue.add(i);
+                }
+            }
+
+            // Then shoot all GREENS
+            for (int i = 0; i < 3; i++) {
+                if (storedColors[i] == NormColorSensor.COLOR.GREEN) {
+                    kickQueue.add(i);
+                }
+            }
+        }
+    }
+
+    //0 = left, 1 = right, 2 = back
     @Override
     public void initialize(){
-        LKSL = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-2");
-        LKSR = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-3");
-        RKSL = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-5");
-        RKSR = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-6");
-        BKSL = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-1");
-        BKSR = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-4");
+        sensors[0][0] = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-2");
+        sensors[0][1] = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-3");
+        sensors[1][0] = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-5");
+        sensors[1][1] = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-6");
+        sensors[2][0] = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-1");
+        sensors[2][1] = new NormColorSensor(ActiveOpMode.hardwareMap(),"clr-4");
     }
 
     @Override
     public void periodic() {
-        colorPositions[0] = LKSL.getDetectedColor();
-        colorPositions[1] = LKSR.getDetectedColor();
-        colorPositions[2] = RKSL.getDetectedColor();
-        colorPositions[3] = RKSR.getDetectedColor();
-        colorPositions[4] = BKSL.getDetectedColor();
-        colorPositions[5] = BKSR.getDetectedColor();
+        sensors[0][0].updateColors();
+        sensors[0][1].updateColors();
+        sensors[1][0].updateColors();
+        sensors[1][1].updateColors();
+        sensors[2][0].updateColors();
+        sensors[2][1].updateColors();
     }
 }
