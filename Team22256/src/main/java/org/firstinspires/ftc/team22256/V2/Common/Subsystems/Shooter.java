56fc4219e9c74026cdf1265b94ac97ba8e0bfa1f
armani-183198 <armani.183198@hastudents.us>
2026-01-26 16:28:44 -0700

    Added velocity PID for flywheel

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Shooter.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/Common/Subsystems/Shooter.java
@@ -1,105 +1,70 @@
 package org.firstinspires.ftc.team22256.V2.Common.Subsystems;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.team22256.V2.Common.Global;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.delays.WaitUntil;
 import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.powerable.SetPower;
+
 
 public class Shooter implements Subsystem {
+    static ControlSystem controlSystem;
     public static final Shooter INSTANCE = new Shooter();
-    private Shooter(){ }
+    private Shooter(){
+         controlSystem = ControlSystem.builder()
+                .velPid(0.0005,0,0)
+                 .basicFF(0.005)
+                .build();
+         controlSystem.setGoal(new KineticState(0,0));
+    }
 
-    public static final MotorEx shooterMotor = new MotorEx("shooter");
-    public static double target = 1700;
-    public static final double farPower = 0;
-    public static final double shortPower = 0;
+    public static final MotorEx shooterLeft = new MotorEx("shooterLeft");
+    public static final MotorEx shooterRight = new MotorEx("shooterRight");
+
+    public static double target = 0;
+
+    public static double distance;
 
     public static double tolerance = 40;
-    public static double kP;
-    public static double kV;
-    public static double kS;
-
-    public static double getVelocity() {
-        return shooterMotor.getVelocity();
+    public static double getLeftVelocity() {
+        return shooterLeft.getVelocity();
+    }
+    public static double getRightVelocity() {
+        return shooterRight.getVelocity();
     }
 
     public static boolean upToSpeed() {
-        return getVelocity() >= target - tolerance;
+        return (Math.abs(getLeftVelocity())+Math.abs(getRightVelocity()))/2 >= target - tolerance;
     }
-    public static Command setFarPower(){
-        target = farPower;
-        return new InstantCommand(() -> shooterMotor.setPower(farPower));
-    }
-    public static Command setShortPower(){
-        target = shortPower;
-        return new InstantCommand(() -> shooterMotor.setPower(shortPower));
-    }
+    public static Command farTriangle = new LambdaCommand()
+            .setStart(() -> {
+                controlSystem.setGoal(new KineticState(0,
+                        265));
+            });
+    //short:200
 
-    public static Command shoot3Far() {
-        return new SequentialGroup(
-                shootFarBK(),
-                shootFarLK(),
-                shootFarRK()
-        );
-    }
-    public static Command shoot3Short() {
-        return new SequentialGroup(
-                shootShortBk(),
-                shootShortLk(),
-                shootShortRk()
-        );
-    }
+    public static Command closeTriangle = new LambdaCommand()
+            .setStart(() -> {
+                controlSystem.setGoal(new KineticState(0,215));
+            })
+            .setIsDone(() -> true);
 
-
-    private static Command shootFarBK() {
-        return new SequentialGroup(
-                new WaitUntil(Shooter::upToSpeed),
-                Sorter.BK_Up,
-                Sorter.BK_Down
-        );
-    }
-    private static Command shootShortBk() {
-        return new SequentialGroup(
-                new WaitUntil(Shooter::upToSpeed),
-                Sorter.BK_Up,
-                Sorter.BK_Down
-        );
-    }
-    private static Command shootFarLK() {
-        return new SequentialGroup(
-                new WaitUntil(Shooter::upToSpeed),
-                Sorter.LK_Up,
-                Sorter.LK_Down
-        );
-    }
-    private static Command shootShortLk() {
-        return new SequentialGroup(
-                new WaitUntil(Shooter::upToSpeed),
-                Sorter.LK_Up,
-                Sorter.LK_Down
-        );
-    }
-    private static Command shootFarRK() {
-        return new SequentialGroup(
-                new WaitUntil(Shooter::upToSpeed),
-                Sorter.BK_Up,
-                Sorter.BK_Down
-        );
-    }
-    private static Command shootShortRk() {
-        return new SequentialGroup(
-                new WaitUntil(Shooter::upToSpeed),
-                Sorter.BK_Up,
-                Sorter.BK_Down
-        );
-    }
 
 
     @Override
     public void periodic() {
+        shooterLeft.setPower(-controlSystem.calculate(shooterRight.getState()));
+        shooterRight.setPower(controlSystem.calculate(shooterRight.getState()));
     }
 
 }
