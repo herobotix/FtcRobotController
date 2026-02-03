f2b48686b6547726cd37ab1cd6d266e384476e48
armani-183198 <armani.183198@hastudents.us>
2026-02-02 18:02:10 -0700

    Configured opmode for testing color sensors

--- Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/OpMode/Testing/ColorSensorTest.java
+++ Team22256/src/main/java/org/firstinspires/ftc/team22256/V2/OpMode/Testing/ColorSensorTest.java
@@ -8,58 +8,33 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
+import org.firstinspires.ftc.team22256.V2.Common.Subsystems.Sorter;
+
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+
 
 @TeleOp
 
-public class ColorSensorTest extends LinearOpMode {
+public class ColorSensorTest extends NextFTCOpMode {
 
-NormalizedColorSensor sensor6, sensor1, sensor2, sensor3, sensor4, sensor5;
-public enum COLOR_DETECTED{
-    RED,
-    GREEN,
-    BLUE,
-    UNKNOWN;
-}
+    {
+        addComponents(
+                BindingsComponent.INSTANCE,
+                new SubsystemComponent(Sorter.INSTANCE)
+        );
+    }
 
     @Override
-    public void runOpMode() {
+    public void onUpdate(){
 
-        sensor1 = hardwareMap.get(NormalizedColorSensor.class,"clr-1");
-        sensor2 = hardwareMap.get(NormalizedColorSensor.class,"clr-2");
-        sensor3 = hardwareMap.get(NormalizedColorSensor.class,"clr-3");
-        sensor4 = hardwareMap.get(NormalizedColorSensor.class,"clr-4");
-        sensor5 = hardwareMap.get(NormalizedColorSensor.class,"clr-5");
-        sensor6 = hardwareMap.get(NormalizedColorSensor.class,"clr-6");
-
-        sensor1.setGain(4);
-        sensor4.setGain(4);
-
-
-
-        telemetry.addData("Status", "Initialized");
+        telemetry.addData("spot 0 color", Sorter.updateSpotColor(0));
+        telemetry.addData("spot 1 color", Sorter.updateSpotColor(1));
+        telemetry.addData("spot 2 color", Sorter.updateSpotColor(2));
         telemetry.update();
-        waitForStart();
 
-        while (opModeIsActive()) {
-            NormalizedRGBA colors1 = sensor1.getNormalizedColors();
-            NormalizedRGBA colors2 = sensor2.getNormalizedColors();
-            NormalizedRGBA colors3 = sensor3.getNormalizedColors();
-            NormalizedRGBA colors4 = sensor4.getNormalizedColors();
-            NormalizedRGBA colors5 = sensor5.getNormalizedColors();
-            NormalizedRGBA colors6 = sensor6.getNormalizedColors();
-
-            telemetry.addData("r1:",colors1.red);
-            telemetry.addData("g1:",colors1.green);
-            telemetry.addData("b1:",colors1.blue);
-            telemetry.addData("r4",colors4.red);
-            telemetry.addData("g4",colors4.green);
-            telemetry.addData("b4",colors4.blue);
-            telemetry.addData("rA",(colors4.red + colors1.red)/2);
-            telemetry.addData("gA",(colors4.green + colors1.green)/2);
-            telemetry.addData("bA",(colors4.blue + colors1.blue)/2);
-
-            telemetry.update();
-
-        }
     }
+
+
 }
