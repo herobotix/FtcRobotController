64fdd1e0a85599ad399b113b5b7a27600152be9a
Lucas-183119 <lucas.183119@hastudents.us>
2026-02-04 16:46:49 -0700

    Update Auto* to Version "1.2.11"**
    Made the Intake stay on throughout the Auto, so that the Outtake will actually fire.
    *Auto is still split into "Red" and "Blue".
    ** Blue's version number labeled "1.2.11a" to maintain an equal version number for both sides.

--- Team22258/src/main/java/org/firstinspires/ftc/team22258/teamcode/opmode_Auto_Blue.java
+++ Team22258/src/main/java/org/firstinspires/ftc/team22258/teamcode/opmode_Auto_Blue.java
@@ -19,7 +19,7 @@ import org.firstinspires.ftc.team22258.pedroPathing.Constants;
 import org.firstinspires.ftc.team22258.teamcode.classes.LIMELIGHT;
 
 
-@Autonomous(name = "Opmode (Auto, Blue) [1.2.11]", group = "Autonomous")
+@Autonomous(name = "Opmode (Auto, Blue) [1.2.11a]", group = "Autonomous")
 @Configurable
 public class opmode_Auto_Blue extends LinearOpMode {
   
@@ -387,95 +387,87 @@ public class opmode_Auto_Blue extends LinearOpMode {
     panelsTelemetry.update(telemetry);
   }
   
-  public void autonomousPathUpdate() {
-    switch (pathState) {
-      case SETUP:     if (isTimerSecsOver(0)) {
+  public void autonomousPathUpdate() { switch (pathState) {
+    case SETUP:     if (isTimerSecsOver(0)) {
+      setOtkServoPos(ServoState.CLOSED);
+      OtkMotor.setPower(0);
+      follower.followPath(paths.SETUP, true);
+      setPathState(PathState.OUTTAKE0);
+    } break;
+    case OUTTAKE0:  if (!follower.isBusy()) {
+      OtkMotor.setPower(0.67);
+      follower.followPath(paths.OUTTAKE0,true);
+      setPathState(PathState.FIRE0);
+    } break;
+    case FIRE0:     if (!follower.isBusy()) {
+      setOtkServoPos(ServoState.OPEN);
+      setPathState(PathState.ALIGN1);
+    } break;
+    case ALIGN1:    if (isTimerSecsOver(2)) {
+      setOtkServoPos(ServoState.CLOSED);
+      OtkMotor.setPower(0);
+      follower.followPath(paths.ALIGN1,true);
+      setPathState(PathState.INTAKE1);
+    } break;
+    case INTAKE1:   if (!follower.isBusy()) {
+      follower.followPath(paths.INTAKE1);
+      setPathState(PathState.OUTTAKE1);
+    } break;
+    case OUTTAKE1:  if (!follower.isBusy()) {
+      OtkMotor.setPower(0.67);
+      follower.followPath(paths.OUTTAKE1,true);
+      setPathState(PathState.FIRE1);
+    } break;
+    case FIRE1:     if (!follower.isBusy()) {
+      setOtkServoPos(ServoState.OPEN);
+      setPathState(PathState.ALIGN2);
+    } break;
+    case ALIGN2:    if (isTimerSecsOver(2)) {
+      setOtkServoPos(ServoState.CLOSED);
+      OtkMotor.setPower(0);
+      follower.followPath(paths.ALIGN2,true);
+      setPathState(PathState.INTAKE2);
+    } break;
+    case INTAKE2:   if (!follower.isBusy()) {
+      follower.followPath(paths.INTAKE2);
+      setPathState(PathState.OUTTAKE2);
+    } break;
+    case OUTTAKE2:  if (!follower.isBusy()) {
+      OtkMotor.setPower(0.67);
+      follower.followPath(paths.OUTTAKE2, true);
+      setPathState(PathState.FIRE2);
+    } break;
+    case FIRE2:     if (!follower.isBusy()) {
+      setOtkServoPos(ServoState.OPEN);
+      setPathState(PathState.ALIGN3);
+    } break;
+    case ALIGN3:    if (isTimerSecsOver(2)) {
+      setOtkServoPos(ServoState.CLOSED);
+      OtkMotor.setPower(0);
+      follower.followPath(paths.ALIGN3,true);
+      setPathState(PathState.INTAKE3);
+    } break;
+    case INTAKE3:   if (!follower.isBusy()) {
+      follower.followPath(paths.INTAKE3);
+      setPathState(PathState.OUTTAKE3);
+    } break;
+    case OUTTAKE3:  if (!follower.isBusy()) {
+      OtkMotor.setPower(0.67);
+      follower.followPath(paths.OUTTAKE3, true);
+      setPathState(PathState.FIRE3);
+    } break;
+    case FIRE3:     if (!follower.isBusy()) {
+      setOtkServoPos(ServoState.OPEN);
+      setPathState(PathState.ALIGN4);
+    } break;
+    case ALIGN4:    if (isTimerSecsOver(2)) {
         setOtkServoPos(ServoState.CLOSED);
-        OtkMotor.setPower(0);
-        follower.followPath(paths.SETUP, true);
-        setPathState(PathState.OUTTAKE0);
-      } break;
-      case OUTTAKE0:  if (!follower.isBusy()) {
-        OtkMotor.setPower(0.67);
         ItkMotor.setPower(0);
-        follower.followPath(paths.OUTTAKE0,true);
-        setPathState(PathState.FIRE0);
-      } break;
-      case FIRE0:     if (!follower.isBusy()) {
-        setOtkServoPos(ServoState.OPEN);
-        setPathState(PathState.ALIGN1);
-      } break;
-      case ALIGN1:    if (isTimerSecsOver(2)) {
-        setOtkServoPos(ServoState.CLOSED);
-        OtkMotor.setPower(0);
-        follower.followPath(paths.ALIGN1,true);
-        setPathState(PathState.INTAKE1);
-      } break;
-      case INTAKE1:   if (!follower.isBusy()) {
-        ItkMotor.setPower(1);
-        follower.followPath(paths.INTAKE1);
-        setPathState(PathState.OUTTAKE1);
-      } break;
-      case OUTTAKE1:  if (!follower.isBusy()) {
-        OtkMotor.setPower(0.67);
-        ItkMotor.setPower(0);
-        follower.followPath(paths.OUTTAKE1,true);
-        setPathState(PathState.FIRE1);
-      } break;
-      case FIRE1:     if (!follower.isBusy()) {
-        setOtkServoPos(ServoState.OPEN);
-        setPathState(PathState.ALIGN2);
-      } break;
-      case ALIGN2:    if (isTimerSecsOver(2)) {
-        setOtkServoPos(ServoState.CLOSED);
-        OtkMotor.setPower(0);
-        follower.followPath(paths.ALIGN2,true);
-        setPathState(PathState.INTAKE2);
-      } break;
-      case INTAKE2:   if (!follower.isBusy()) {
-        ItkMotor.setPower(1);
-        follower.followPath(paths.INTAKE2);
-        setPathState(PathState.OUTTAKE2);
-      } break;
-      case OUTTAKE2:  if (!follower.isBusy()) {
-        OtkMotor.setPower(0.67);
-        ItkMotor.setPower(0);
-        follower.followPath(paths.OUTTAKE2, true);
-        setPathState(PathState.FIRE2);
-      } break;
-      case FIRE2:     if (!follower.isBusy()) {
-        setOtkServoPos(ServoState.OPEN);
-        setPathState(PathState.ALIGN3);
-      } break;
-      case ALIGN3:    if (isTimerSecsOver(2)) {
-        setOtkServoPos(ServoState.CLOSED);
-        OtkMotor.setPower(0);
-        follower.followPath(paths.ALIGN3,true);
-        setPathState(PathState.INTAKE3);
-      } break;
-      case INTAKE3:   if (!follower.isBusy()) {
-        ItkMotor.setPower(1);
-        follower.followPath(paths.INTAKE3);
-        setPathState(PathState.OUTTAKE3);
-      } break;
-      case OUTTAKE3:  if (!follower.isBusy()) {
-        OtkMotor.setPower(0.67);
-        ItkMotor.setPower(0);
-        follower.followPath(paths.OUTTAKE3, true);
-        setPathState(PathState.FIRE3);
-      } break;
-      case FIRE3:     if (!follower.isBusy()) {
-        setOtkServoPos(ServoState.OPEN);
-        setPathState(PathState.ALIGN4);
-      } break;
-      case ALIGN4:    if (isTimerSecsOver(2)) {
-        setOtkServoPos(ServoState.CLOSED);
         OtkMotor.setPower(0);
         follower.followPath(paths.ALIGN4, true);
         setPathState(PathState.END);
       } break;
-    }
-  }
+  } }
   
   /** These change the states of the paths and actions. It will also reset the timers of the individual switches **/
   private void setPathState(PathState pState) {
