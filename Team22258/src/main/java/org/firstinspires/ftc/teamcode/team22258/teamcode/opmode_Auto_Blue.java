4f5633ef945bfead8ede456d9b0a8d4c90ddb856
Lucas-183119 <lucas.183119@hastudents.us>
2026-09-25 13:55:52 -0700

    Fix Teamcode to work for new season
    Changes made at states last year were not commited before the change to the team repository. This fixes errors made from the commits

--- Team22258/src/main/java/org/firstinspires/ftc/teamcode/team22258/teamcode/opmode_Auto_Blue.java
+++ Team22258/src/main/java/org/firstinspires/ftc/teamcode/team22258/teamcode/opmode_Auto_Blue.java
@@ -1,36 +1,14 @@
-package org.firstinspires.ftc.teamcode.team22258.teamcode;
+package org.firstinspires.ftc.team22258.teamcode;
 
-import com.bylazar.configurables.annotations.Configurable;
+import static org.firstinspires.ftc.teamcode.team22258.teamcode.classes.AUTO.Version;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import org.firstinspires.ftc.teamcode.team22258.teamcode.classes.AUTO;
 
-@Autonomous(name = "Opmode (Auto, Blue) [1.2.22]", group = "Autonomous")
-@Configurable
-public class opmode_Auto_Blue extends LinearOpMode {
-  //Autonomous Opmode on Blue
-  
-  // Run Function
-    @Override
-    public void runOpMode() {
-      //Begin
-      
-      // Class Definitions
-        AUTO auto = new AUTO(telemetry, false);
-        
-      // Init & Wait
-        auto.Init(hardwareMap);
-        waitForStart();
-        
-      // Run Opmode
-        while (opModeIsActive()) {
-          auto.doLoop();
-          if (gamepad1.a) {
-            while (opModeIsActive()) {}
-          }
-        }
-        
-    }
-    
+/**Autonomous Opmode on Blue*/
+@Autonomous(name = "Opmode (Auto, Blue) ["+ Version +"]", group = "Autonomous")
+public class opmode_Auto_Blue extends AUTO {
+  public opmode_Auto_Blue() {
+    isRedAlliance = false;
+  }
 }
